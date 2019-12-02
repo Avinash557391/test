44a360fd8b35f4e3058963caa180cf10abdce3c5
dell <dell@DESKTOP-T43RPRT>
2019-12-02 15:34:05 +0530

    added one prop

--- Github/src/com/rs/git/Git.java
+++ Github/src/com/rs/git/Git.java
@@ -1,22 +1,33 @@
 package com.rs.git;
 
 public class Git {
-	
 
-private int id;
-private String name;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-		
+	private int id;
+	private String name;
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
 
 }
